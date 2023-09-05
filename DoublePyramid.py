9cef90632e1665af27c1f9786581a86f2cbb553c
Abeer-Khalifa <139794587+Abeer-Khalifa@users.noreply.github.com>
2023-09-06 01:11:41 +0300

    Delete DoublePyramid.py

--- DoublePyramid.py
+++
@@ -1,9 +0,0 @@
-n=0
-while n>8 or n<1:
-    n=int(input("Input: "))
-    if n>8:
-        print("Number must be smaller than or equal to 8")
-    elif n<1:
-        print("Number must be greater than or equal to 1")
-for i in range (n):
-    print (" "*(n-i-1) + "#"*(i+1) +"  " + "#"*(i+1) )
